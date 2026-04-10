eddff27023ebce39da5dce11b8bfec24eaafbace
tiffany mcclain <forman.syah@yahoo.com>
2026-04-10 23:22:05 +0700

    Update config.py

--- config.py
+++ config.py
@@ -76,7 +76,7 @@ class InferenceConfig:
     resnap_events: bool = True  # Resnap notes to the timing after generation
 
     # Metadata settings
-    bpm: Optional[int] = None  # Beats per minute of input audio
+    bpm: Optional[int] = 150  # Beats per minute of input audio
     offset: Optional[int] = None  # Start of beat, in miliseconds, from the beginning of input audio
     title: Optional[str] = None  # Song title
     title_unicode: Optional[str] = None  # Song title in Unicode/Japanese
